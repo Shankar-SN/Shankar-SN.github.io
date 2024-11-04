6083d1153d14ab7f5b55464f72682b5a955f9640
Shankar-SN <chelseaindru@gmail.com>
2024-11-04 09:02:37 -0600

    Made chnages to the portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
